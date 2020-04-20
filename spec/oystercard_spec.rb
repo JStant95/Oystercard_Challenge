dc4b2c78c476d13b8483fdb703cc6a164611ca7a
Ed <lookatemail@gmail.com>
2020-04-20 15:27:22 +0100

    added method to deduct from current balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,29 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  before(:each) { subject.top_up(5) }
   
   it '#balence of the new card should be equal 0' do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq 5
   end
 
   it '#top_up should be able to add to the balance' do
-    subject.top_up(5)
     expect(subject.balance).to eq 5
   end
 
   it '#top_up should able to add to curent balnce' do
     2.times { subject.top_up(15) }
-    expect(subject.balance).to eq 30
+    expect(subject.balance).to eq 35
   end
 
   it 'should have balance limit of 90' do
     message = "Balance limit reached: #{Oystercard::LIMIT}"
     expect { subject.top_up(91) }.to raise_error message
+  end
+
+  it 'should be able to #deduct from balance' do
+    expect(subject.deduct(5)).to eq 0
   end
 
 end
